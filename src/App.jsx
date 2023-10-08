247496df035123e9621d7a05c1d1082c9e8a6550
Ian Kamau <kian99564@gmail.com>
2023-10-08 13:12:53 +0300

    Starter

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  useEffect(() => {
+    fetch("http://localhost:3000/posts/", {
+      // method: "POST",
+      // body: JSON.stringify({
+      //   id: 1,
+      //   title: "First",
+      // }),
+      // headers: {
+      //   "Content-type": "application/json; charset=UTF-8",
+      // },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
