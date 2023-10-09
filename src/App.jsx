127959fa526934cf2b1ef82f1591f581ab895540
Ian Kamau <kian99564@gmail.com>
2023-10-09 20:07:11 +0300

    Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,6 @@ function App() {
   useEffect(() => {
     fetch("http://localhost:3000/posts/", {
       method: "GET",
-      // body: JSON.stringify({
-      //   id: 6,
-      //   title: "First",
-      // }),
       // headers: {
       //   "Content-type": "application/json; charset=UTF-8",
       // },
